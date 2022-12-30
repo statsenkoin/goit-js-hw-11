e8efbfceba45488bf1c951151b1252f6976fe77a
Ihor Statsenko <statsenkoin@gmail.com>
2022-12-30 21:00:20 +0200

    fetch data useing axios

--- src/index.js
+++ src/index.js
@@ -1,1 +1,39 @@
-console.log('Homework 11 :>> ', 'Homework 11');
+import { fetchPixabay } from './js/fetchPixabay';
+
+const form = document.querySelector('#search-form');
+form.addEventListener('submit', onSearch);
+
+let page = 1;
+
+// submit
+async function onSearch(event) {
+  event.preventDefault();
+  //   const {
+  //     elements: { searchQuery },
+  //   } = event.currentTarget;
+  //   const search = searchQuery.value;
+  const search = event.currentTarget.elements.searchQuery.value;
+  const res = await getData(search, page);
+  createMarkup(res);
+  console.log('res :>> ', res);
+}
+
+// fetch
+async function getData(search, page) {
+  try {
+    const responce = await fetchPixabay(search, page);
+    return responce.data.hits;
+  } catch (error) {
+    console.log('error :>> ', error.message);
+  }
+}
+
+function createMarkup(res) {}
+
+// webformatURL - посилання на маленьке зображення для списку карток.
+// largeImageURL - посилання на велике зображення.
+// tags - рядок з описом зображення. Підійде для атрибуту alt.
+// likes - кількість лайків.
+// views - кількість переглядів.
+// comments - кількість коментарів.
+// downloads - кількість завантажень.
---
+++ src/js/fetchPixabay.js
@@ -0,0 +1,51 @@
+import axios from 'axios';
+
+const API_KEY = '32468715-2ee7d1cef437ed65ce73ff92a';
+const BASE_URL = 'https://pixabay.com/api/';
+
+const params = new URLSearchParams({
+  key: API_KEY,
+  image_type: 'photo',
+  orientation: 'horizontal',
+  safesearch: true,
+  per_page: 40,
+});
+
+export async function fetchPixabay(search, page) {
+  return await axios.get(`${BASE_URL}?q=${search}&page=${page}`, { params });
+}
+
+// ========================================================================
+// const params = new URLSearchParams({
+//   image_type: 'photo',
+//   orientation: 'horizontal',
+//   safesearch: true,
+//   per_page: 40,
+// });
+
+// export async function fetchPixabay(search, page) {
+//   return await axios.get(
+//     `${BASE_URL}?key=${API_KEY}&q=${search}&page=${page}&${params}`
+//   );
+// }
+
+// =========================================================================
+// export async function fetchPixabay(search, page) {
+//   const responce = await fetch(
+//     `${BASE_URL}?key=${API_KEY}&q=${search}&${params}&page=${page}`
+//   );
+//   if (!responce.ok) throw new Error(responce.statusText);
+//   return await responce.json();
+// }
+
+// export async function fetchPixabay(search, page) {
+//   try {
+//     const responce = await axios.get(
+//       `${BASE_URL}?key=${API_KEY}&q=${search}&${params}&page=${page}`
+//     );
+//     console.log('responce :>> ', responce);
+//     return responce;
+//   } catch (error) {
+//     console.log('error :>> ', error);
+//   }
+// }
