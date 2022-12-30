53ea14d36d6d1c81192b3d6c905fa58b375da016
statsenkoin <34393845+statsenkoin@users.noreply.github.com>
2022-12-30 19:00:51 +0000

    Deploying to gh-pages from @ statsenkoin/goit-js-hw-11@e8efbfceba45488bf1c951151b1252f6976fe77a 🚀

--- index.2b3c41e6.js
+++
@@ -1,2 +0,0 @@
-console.log("Homework 11 :>> ","Homework 11");
-//# sourceMappingURL=index.2b3c41e6.js.map
--- index.7951d927.js
+++
@@ -1,2 +0,0 @@
-console.log("Homework 11 :>> ","Homework 11");
-//# sourceMappingURL=index.7951d927.js.map
