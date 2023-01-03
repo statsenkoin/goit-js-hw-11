03b96b5712d8c0f355ef8cdccb3177e8158eccd9
Ihor Statsenko <statsenkoin@gmail.com>
2023-01-03 23:05:14 +0200

    disable title when show large image

--- src/js/simple_lightbox.js
+++ src/js/simple_lightbox.js
@@ -2,7 +2,7 @@ import SimpleLightbox from 'simplelightbox';
 import 'simplelightbox/dist/simple-lightbox.min.css';
 
 export const simpleLightbox = new SimpleLightbox('.gallery a', {
-  captionsData: 'alt',
+  // captionsData: 'alt',
   captionDelay: 250,
   close: true,
 });
