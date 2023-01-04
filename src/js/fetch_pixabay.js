b1033dcce35d309c7ab43be04423d13714a1d65c
Ihor Statsenko <statsenkoin@gmail.com>
2023-01-04 11:18:05 +0200

    clean up fetch_pixabay.js

--- src/js/fetch_pixabay.js
+++ src/js/fetch_pixabay.js
@@ -16,38 +16,3 @@ export async function fetchPixabay(search, page, perPage) {
     { params }
   );
 }
-
-// ========================================================================
-// const params = new URLSearchParams({
-//   image_type: 'photo',
-//   orientation: 'horizontal',
-//   safesearch: true,
-//   per_page: 40,
-// });
-
-// export async function fetchPixabay(search, page) {
-//   return await axios.get(
-//     `${BASE_URL}?key=${API_KEY}&q=${search}&page=${page}&${params}`
-//   );
-// }
-
-// =========================================================================
-// export async function fetchPixabay(search, page) {
-//   const responce = await fetch(
-//     `${BASE_URL}?key=${API_KEY}&q=${search}&${params}&page=${page}`
-//   );
-//   if (!responce.ok) throw new Error(responce.statusText);
-//   return await responce.json();
-// }
-
-// export async function fetchPixabay(search, page) {
-//   try {
-//     const responce = await axios.get(
-//       `${BASE_URL}?key=${API_KEY}&q=${search}&${params}&page=${page}`
-//     );
-//     console.log('responce :>> ', responce);
-//     return responce;
-//   } catch (error) {
-//     console.log('error :>> ', error);
-//   }
-// }
