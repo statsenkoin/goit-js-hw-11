2f4b3cc1a709d5e2546c901b039b40f93147aaae
Ihor Statsenko <statsenkoin@gmail.com>
2023-01-04 11:18:46 +0200

    adds options for notify messages

--- src/js/notify_message.js
+++ src/js/notify_message.js
@@ -1,16 +1,28 @@
 import { Notify } from 'notiflix';
 
+const options = {
+  width: '280px',
+  position: 'left-top', // 'right-top' - 'right-bottom' - 'left-top' - 'left-bottom' - 'center-top' - 'center-bottom' - 'center-center'
+  distance: '40px',
+  cssAnimationDuration: 800,
+  cssAnimationStyle: 'from-left', // 'fade' - 'zoom' - 'from-right' - 'from-top' - 'from-bottom' - 'from-left'
+};
+
 export function showMessage(searchResult, page, pages) {
   const totalHits = searchResult.totalHits;
   if (page === 1 && searchResult.hits.length) {
-    Notify.success(`Hooray! We found ${totalHits} images.`);
+    Notify.success(`Hooray! We found ${totalHits} images.`, options);
   }
   if (!searchResult.hits.length) {
     Notify.failure(
-      'Sorry, there are no images matching your search query. Please try again.'
+      'Sorry, there are no images matching your search query. Please try again.',
+      options
     );
   }
   if (pages === page) {
-    Notify.info("We're sorry, but you've reached the end of search results.");
+    Notify.info(
+      "We're sorry, but you've reached the end of search results.",
+      options
+    );
   }
 }
